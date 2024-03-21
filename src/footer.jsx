6faa8d78d7b28931a638e34967ebd6f9c986c622
Haneesh19 <haneesh1901@gmail.com>
2024-03-21 14:26:15 +0530

    footer Updated

--- src/footer.jsx
+++ src/footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     return (
         <footer className="py-5 bg-dark">
             <div className="container">
-                <p className="m-0 text-center text-white">Copyright &copy; Your Website 2024</p>
+                <p className="m-0 text-center text-white">Copyright &copy; Shopping-Cart 2024</p>
             </div>
         </footer>
     )
